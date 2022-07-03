b2a9aeef3cec2172bc7f0051c3e20fd3460941d1
Josh <97398293+joshjkk@users.noreply.github.com>
2022-07-03 14:35:39 +0100

    Added if __name__ == "__main__"

--- bot.py
+++ bot.py
@@ -1,18 +1,27 @@
-import discord, os
+import discord
 from discord.ext import commands
+import os
 
 bot = commands.Bot(command_prefix="-", help_command=None, intents=discord.Intents.all())
 
-# Return token from a seperate .txt file
+# Get bot token from a file called 'token.txt'
 def get_token():
     with open("./token.txt", 'r') as file:
         return file.readlines()[0].strip()
 
-# Load cogs from a folder named 'cogs'
-for filename in os.listdir("./cogs"):
-    if filename.endswith('.py'):
-        bot.load_extension(f"cogs.{filename[:-3]}")
-    else:
-        print(f"Unable to load {filename[:-3]}")
+# Load all .py files from a folder named 'cogs'
+def load_cogs():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith('.py'):
+            bot.load_extension(f"cogs.{filename[:-3]}")
+        else:
+            print(f"Unable to load {filename[:-3]}")
 
-bot.run(get_token())
+
+def main():
+    load_cogs()
+    bot.run(get_token())
+
+# Run if the file is being ran directly
+if __name__ == "__main__":
+    main()
